fff90ac204965593e54ad18025739c4e097ae688
tainamrny <tad-tainatalarico@ucpparana.edu.br>
2026-07-23 21:46:47 -0300

    Add files via upload

---
+++ Aula_23_07.py
@@ -0,0 +1,10 @@
+def soma (a, b):
+  resultado= a+b
+  print (resultado)
+   
+  return resultado
+
+soma (2, 3)
+
+# função parametrizada (recebe valores de entrada)
+# stack: pilha de execução, o último item a ser colocado na lista é o primeiro a ser retirado (LIFO LAST IN, FIRST OUT)
